61ec99df8fc72acde52bc19ceb9f1a16f519baba
naterush <naterush1997@gmail.com>
2019-10-30 01:43:56 -0400

    change references

--- express-server/app.js
+++ express-server/app.js
@@ -20,7 +20,8 @@ app.engine('html', require('ejs').renderFile);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use('/static', express.static('public'))
+//app.use('/static', express.static('public'))
+app.use(express.static('public'))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

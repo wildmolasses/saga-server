a0680fad7d4fdbd945ccd87d00662bf3d36549da
Aaron Diamond-Reivich <aarondia@wharton.upenn.edu>
2020-02-04 21:39:22 -0500

    connected node app to mongodb atlas

--- routes/platform.js
+++ routes/platform.js
@@ -8,6 +8,19 @@ const Feedback = mongoose.model('Feedback');
 const auth = require('./auth');
 var exec = require('child_process').exec, child;
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admin:oNL1sXL6R0jktqVC@cluster0-4sm65.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+
+    var db = client.db('Users');
+
+    db.collection('user').findOne({}, function (findErr, result) {
+        if (findErr) throw findErr;
+        console.log(result);
+        client.close();
+    });
+});
 
 // Render the homePage page
 router.get('/', (req, res) => {
